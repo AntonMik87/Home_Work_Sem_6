405ae8e8db4d914385115e84e12833800c701c57
«Anton» <hranitel-liks@mail.ru>
2022-08-27 02:03:21 +0700

    add HW001

---
+++ Tasks/HW001/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+double[,] matrix = new double[5, 5];
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = new Random().NextDouble() * 100;
+        Console.Write(" {0,6:F2} ", matrix[i, j]);
+    }
+    Console.WriteLine();
+}
---
+++ ZD.md
@@ -0,0 +1,6 @@
+Решить следующие задачи:
+1. https://codeforces.com/contest/1/problem/A
+2. https://codeforces.com/contest/617/problem/A
+3. https://codeforces.com/contest/271/problem/A
+4. https://codeforces.com/contest/110/problem/A
+5. https://codeforces.com/contest/546/problem/A
